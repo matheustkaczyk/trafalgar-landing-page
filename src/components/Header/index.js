e00c091d1e7ed7f460dffefe4db54349bfb5e061
matheustkaczyk <matheus111904@hotmail.com>
2022-09-19 15:45:16 -0300

    feat: componente header estruturaod

---
+++ src/components/Header/index.js
@@ -0,0 +1,18 @@
+const Header = () => {
+  return(
+    <header>
+      <p>logo</p>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Find a doctor</li>
+          <li>Apps</li>
+          <li>Testimonials</li>
+          <li>About us</li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default Header;
