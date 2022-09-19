7b21a3c13f96f26119133590157fb04eeb6f2b7b
matheustkaczyk <matheus111904@hotmail.com>
2022-09-19 16:18:12 -0300

    feat: adicionando imagem home, estruturando page home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,15 @@
+import homeImg from '../../assets/trafalgar-home.svg';
+
 const Home = () => {
   return(
-    <section></section>
+    <section>
+      <div>
+        <h1>Virtual healthcare for you</h1>
+        <p>Trafalgar provides progressive, and affordable healthcare, accessible on mobile and online for everyone</p>
+        <button>Consult today</button>
+      </div>
+      <img src={homeImg} alt="home image" />
+    </section>
   )
 }
 
