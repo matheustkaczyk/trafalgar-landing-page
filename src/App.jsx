6c1b65115208ffae356de4a1741fca6a8126ff69
matheustkaczyk <matheus111904@hotmail.com>
2022-09-19 16:15:50 -0300

    importando page home

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,13 @@ import './App.scss'
 
 import Header from './components/Header'
 
+import Home from './pages/Home'
+
 function App() {
   return (
     <div className="App">
       <Header />
+      <Home />
     </div>
   )
 }
