9beec58841d2db944e0b9c409f5c87a06bbd2084
matheustkaczyk <matheus111904@hotmail.com>
2022-09-26 10:54:00 -0300

    refact: removendo carousel, começando do zero

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.scss'
 
 import Header from './components/Header'
-import Carousel from './components/Carousel'
 
 import Home from './pages/Home'
 import Leading from './pages/Leading'
 import Services from './pages/Services'
+
+import tkaczyk from './assets/tkaczyk.jpg';
 
 function App() {
   return (
@@ -14,9 +15,6 @@ function App() {
       <Home />
       <Services />
       <Leading />
-      <Carousel>
-        
-      </Carousel>
     </div>
   )
 }
