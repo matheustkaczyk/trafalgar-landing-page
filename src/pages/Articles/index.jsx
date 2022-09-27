d9b2bc06262efc14f766d89f091ab93007db05cd
matheustkaczyk <matheus111904@hotmail.com>
2022-09-27 14:16:39 -0300

    feat: estruturando mock de artigos

--- src/pages/Articles/index.jsx
+++ src/pages/Articles/index.jsx
@@ -1,3 +1,28 @@
+import img1 from '../../assets/img1.png';
+import img2 from '../../assets/img2.png';
+import img3 from '../../assets/img3.png';
+
+const data = [
+  {
+    img: img1,
+    alt: 'img1',
+    title: 'Disease detection, check up in the laboratory',
+    text: 'In this case, the role of the health laboratory is very important to detect diseases in the early stages...'
+  },
+  {
+    img: img2,
+    alt: 'img2',
+    title: 'Herbal medicines that are safe for consumption',
+    text: 'Herbal medicine is very widely used at this time because its very good to your health...'
+  },
+  {
+    img: img3,
+    alt: 'img3',
+    title: 'Natural care for healty facial skin',
+    text: 'A healthy lifestyle should start from now and also for your skin health. There are some...'
+  }
+];
+
 const Articles = () => {
   return(
     <section className="articles-section">
