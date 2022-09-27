b92ec53852108cb08a327b7874c5feb5b6087e04
matheustkaczyk <matheus111904@hotmail.com>
2022-09-27 10:57:52 -0300

    fix: adicionando ponto e virgula nos imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
-import './App.scss'
+import './App.scss';
 
-import Header from './components/Header'
+import Header from './components/Header';
 
-import Home from './pages/Home'
-import Leading from './pages/Leading'
-import Services from './pages/Services'
+import Home from './pages/Home';
+import Leading from './pages/Leading';
+import Services from './pages/Services';
 
 import tkaczyk from './assets/tkaczyk.jpg';
+
+const data = [
+  {
+    img: tkaczyk,
+    alt: 'tkaczyk',
+    name: 'Matheus Tkaczyk Ribeiro',
+    subtitle: 'Software Developer',
+    text: 'Our dedicated patient engagement app and web portal allow you to access information instantaneously (no tedeous form, long calls, or administrative hassle) and securely.'
+  },
+  {
+    img: tkaczyk,
+    alt: 'tkaczyk',
+    name: 'Matheus Tkaczyk Ribeiro',
+    subtitle: 'Software Developer',
+    text: 'Our dedicated patient engagement app and web portal allow you to access information instantaneously (no tedeous form, long calls, or administrative hassle) and securely.'
+  },
+]
 
 function App() {
   return (
