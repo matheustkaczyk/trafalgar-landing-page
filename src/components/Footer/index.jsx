744dd14b3302ef87bd9ae257a56aba299476d957
matheustkaczyk <matheus111904@hotmail.com>
2022-09-29 10:08:25 -0300

    refact: alterando media query, aumentando p 880-915

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -10,33 +10,36 @@ const Footer = () => {
           <p>© 2021. All rights reserved</p>
           <p>Construído por Matheus Tkaczyk Ribeiro</p>
         </div>
-        <div>
-          <h4>Company</h4>
-          <ul>
-            <li>About</li>
-            <li>Testimonials</li>
-            <li>Find a doctor</li>
-            <li>Apps</li>
-          </ul>
-        </div>
-        <div>
-          <h4>Region</h4>
-          <ul>
-            <li>Indonesia</li>
-            <li>Singapore</li>
-            <li>Hongkong</li>
-            <li>Canada</li>
-          </ul>
-        </div>
-        <div>
-          <h4>Help</h4>
-          <ul>
-            <li>Help center</li>
-            <li>Contact support</li>
-            <li>Instructions</li>
-            <li>How it works</li>
-          </ul>
-        </div>
+        <nav>
+
+          <div>
+            <h4>Company</h4>
+            <ul>
+              <li>About</li>
+              <li>Testimonials</li>
+              <li>Find a doctor</li>
+              <li>Apps</li>
+            </ul>
+          </div>
+          <div>
+            <h4>Region</h4>
+            <ul>
+              <li>Indonesia</li>
+              <li>Singapore</li>
+              <li>Hongkong</li>
+              <li>Canada</li>
+            </ul>
+          </div>
+          <div>
+            <h4>Help</h4>
+            <ul>
+              <li>Help center</li>
+              <li>Contact support</li>
+              <li>Instructions</li>
+              <li>How it works</li>
+            </ul>
+          </div>
+        </nav>
       </div>
     </footer>
   )
