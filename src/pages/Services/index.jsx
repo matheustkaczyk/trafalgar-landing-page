866d5adffa75cb277f78538d51499902c9bee1de
matheustkaczyk <matheus111904@hotmail.com>
2022-09-29 10:18:41 -0300

    feat: aparentemente responsividade concluida

--- src/pages/Services/index.jsx
+++ src/pages/Services/index.jsx
@@ -43,7 +43,7 @@ const infos = [
 const Services = () => {
   return (
     <section className="services-page">
-      <div>
+      <div className='services-infos'>
         <h1>Our Services</h1>
         <hr />
         <p>We provide to you the best choiches for you. Adjust it to your health needs and make sure your undergo treatment with our highly qualified doctors you can consult with us which type of service is suitable for your health</p>
